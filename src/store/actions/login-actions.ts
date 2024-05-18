b62ebffb9c9c7fa28dfbb40c343a123bd85b2367
Darius <dkumza2c@gmail.com>
2024-05-18 18:26:34 +0300

    axios error helper fn

--- src/store/actions/login-actions.ts
+++ src/store/actions/login-actions.ts
@@ -3,40 +3,44 @@ import axios, { AxiosError } from 'axios';
 
 const AUTH_URL = 'http://127.0.0.1:3000/auth';
 
-export const login = createAsyncThunk(
-  'login/loginUser',
+type MyRejectValue = { message: string; status: number | undefined };
 
-  async (loginData: { email: string; password: string }, thunkAPI) => {
-    try {
-      const response = await axios.post(`${AUTH_URL}/login`, loginData);
-      // console.log('response: ', response.data);
-      return response.data.accessToken;
-    } catch (error: unknown) {
-      const axiosError = error as AxiosError;
-      return thunkAPI.rejectWithValue({
-        message: axiosError.message,
-        status: axiosError.response?.status,
-      });
-    }
+const handleAxiosError = (
+  error: unknown,
+  thunkAPI: { rejectWithValue: (value: MyRejectValue) => void }
+) => {
+  const axiosError = error as AxiosError;
+  return thunkAPI.rejectWithValue({
+    message: axiosError.message,
+    status: axiosError.response?.status,
+  });
+};
+
+export const login = createAsyncThunk<
+  string,
+  { email: string; password: string },
+  { rejectValue: MyRejectValue }
+>('login/loginUser', async (loginData, thunkAPI) => {
+  try {
+    const response = await axios.post(`${AUTH_URL}/login`, loginData);
+    // console.log('response: ', response.data);
+    return response.data.accessToken;
+  } catch (error) {
+    return handleAxiosError(error, thunkAPI);
   }
-);
+});
 
-export const tokenStatus = createAsyncThunk(
+export const tokenStatus = createAsyncThunk<string, string, { rejectValue: MyRejectValue }>(
   'login/tokenStatus',
-
-  async (token: string, thunkAPI) => {
+  async (token, thunkAPI) => {
     try {
       const response = await axios.get(`${AUTH_URL}/status`, {
         headers: { Authorization: `Bearer ${token}` },
       });
 
       return response.data;
-    } catch (error: unknown) {
-      const axiosError = error as AxiosError;
-      return thunkAPI.rejectWithValue({
-        message: axiosError.message,
-        status: axiosError.response?.status,
-      });
+    } catch (error) {
+      return handleAxiosError(error, thunkAPI);
     }
   }
 );

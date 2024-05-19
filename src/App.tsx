a1fc4a0000ff5ddee0eded2754f8f0d29fcc5686
Darius <dkumza2c@gmail.com>
2024-05-19 11:42:38 +0300

    chore: Add 404 route to handle page not found error

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Home } from './components/pages/home/Home';
 import { useAppSelector } from './store/hooks';
 import { ToastThemed } from './components/UI/ToastThemed.tsx';
 import 'react-toastify/dist/ReactToastify.css';
+import { PageNotFound } from './components/pages/pageNotFound/PageNotFound.tsx';
 
 function App() {
   const token = useAppSelector((state) => state.login.token);
@@ -14,8 +15,10 @@ function App() {
       <ToastThemed />
       <Routes>
         <Route path='/' element={token ? <Home /> : <Navigate to='/login' />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/signup' element={<SignUpPage />} />
+        <Route path='/login' element={token ? <Navigate to='/' /> : <LoginPage />} />
+        <Route path='/signup' element={token ? <Navigate to='/' /> : <SignUpPage />} />
+        {/* 404 route */}
+        <Route path='*' element={<PageNotFound />} />
       </Routes>
     </div>
   );
